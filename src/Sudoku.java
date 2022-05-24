8e70ed65c9a0870eab03f12004a4477c62443d4b
Jarrett <jarrettcrump@gmail.com>
2022-05-24 12:05:54 -0500

    test

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -28,7 +28,7 @@ public class Sudoku {
     
     private int[][] displayPuzzle = new int[9][9]; //new array to contain the generated, incomplete puzzle.
     //private int[][] completePuzzle = new int[9][9]; //new array to contain the completed puzzle which can be compared aginst when user is solving.
-    private int[][] completePuzzle = {{3,9,5,1,2,4,7,6,8},{6,8,4,3,5,7,2,1,9},{1 2 7 6 8 9 4 3 5},{},{},{},{},{},{}};
+    private int[][] completePuzzle = {{0,0,0,1,0,4,0,6,8},{6,0,0,3,0,0,2,1,9},{1,2,0,0,8,9,4,3,5},{4,0,0,2,0,3,0,8,1},{8,1,0,0,9,0,0,2,4},{0,0,2,0,1,8,0,7,0},{9,0,0,5,0,0,1,0,3},{2,4,0,0,3,0,0,5,0},{0,0,0,8,0,1,0,0,2}};
     public int count = 0;
 
     Sudoku() {
@@ -41,7 +41,7 @@ public class Sudoku {
         //generateUnitRandom(8);
 
         solve();
-
+        System.out.print(count);
         //printPuzzle();
     }
 
